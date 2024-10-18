d74b7e05105aed699018ab33f1af2b77246bade8
Godinhoweverson <weverson.godinho@icloud.com>
2024-10-18 16:26:56 +0100

    Create displayQuestions function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 const pickSubjectBtn = document.querySelectorAll('.pick a');
-const title = document.querySelector('.question-type p')
-const iconSubject = document.querySelector('.question-type')
+const titleDisplay = document.querySelector('.question-type p');
+const iconSubject = document.querySelector('.question-type');
+const questionsHeanding = document.querySelector('.questions h1');
+const btnOptionsParagraph = document.querySelectorAll('.answers p');
+const btnOptions = document.querySelectorAll('.answers');
 
 // SLIDER
 const range = () =>{
@@ -21,26 +24,20 @@ const btnValue = () =>{
     el.addEventListener('click',()=>{
       let valueSubject = el.textContent
       localStorage.setItem('selectedSubject', valueSubject);
-      let storageRemove = true
-      localStorageRemove(storageRemove)
     })
   })
 }
 btnValue()
 
+
 // Get the Subject selected from localStorage
 const selectedSubject = localStorage.getItem('selectedSubject');
-
-const localStorageRemove = (){
-  if(selectedSubject){
-    if(slider){
-    localStorage.removeItem('selectedSubject');
-    }
-  }
+if(selectedSubject){
+  localStorage.removeItem('selectedSubject');
 }
+   
 
-
-    // . Get the data from Json 
+// . Get the data from Json 
 let data;
 fetch('./data.json')
   .then((res) => res.json())
@@ -54,21 +51,30 @@ const quizzes = () =>{
   for(items of Object.entries(data)){
     for(let i = 0; i < 4; i++){
       if(items[1][i].title === selectedSubject){
-        title.innerHTML = items[1][i].title
+        titleDisplay.innerHTML = items[1][i].title
         const img = document.createElement('img')
         img.src = `${items[1][i].icon}`
         img.style = 'width:56px; heigth:56px;'
         iconSubject.prepend(img)
+        displayQuestions(items[1][i].questions)
       }
     }
   }
 }
 
+const displayQuestions = (questions) => {
+  questionsHeanding.innerHTML = questions[0].question
+  for(let i = 0; i < btnOptions.length; i++){
+    btnOptionsParagraph[i].innerHTML = questions[0].options[i]
+  }
+}
 
+btnOptions.forEach(element => {
+  element.addEventListener('click', (btn) =>{
+    console.log(btn)
+  })
+});
 
-// Create a loop to iterate on 4 properties and check if is true or false
-// if is true print the title, icon, and question
+// Get the options button and add a eventListener 
 
-
-// get the class from Html for Icon , and title and display on the page
-
+// and print the value
