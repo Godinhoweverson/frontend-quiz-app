ed559072a938c8aa6a06551c454c6fd17d328f63
Weverson Godinho <weverson.godinho@icloud.com>
2024-10-22 15:23:54 +0000

    set the properties when the answer is wrong

--- assets/js/script.js
+++ assets/js/script.js
@@ -76,6 +76,7 @@ const displayQuestions = (questions) => {
 }
 
 // Get value from Answer selected
+
 const getValueBtnOptions = (answer)=>{
   answer.forEach(el => {
     el.addEventListener('click', () =>{
@@ -99,16 +100,19 @@ const getValueBtnOptions = (answer)=>{
 
 // Submit the answer
 const submitAnswerBtn = (elementSelected,selectedAnswer) =>{
-    submitAnswer.addEventListener('click',() =>{
-      answerIsCorrect(correctAnswer, selectedAnswer, elementSelected)
-    })
+  submitAnswer.addEventListener('click',() =>{
+    answerIsCorrect(correctAnswer, selectedAnswer, elementSelected)
+  
+  })
 }
 
 const answerIsCorrect = (correctAnswer, selectedAnswer, elementSelected) => {
   if(correctAnswer.answer === selectedAnswer){
-    elementSelected.parentNode.style = 'border: 2px solid #26D782;'
+    // CORRECT ANSWER
+    elementSelected.parentNode.style = 'border: 2px solid #26D782; justify-content:space-between;';
     elementSelected.previousElementSibling.style.backgroundColor = '#26D782';
     const div = document.createElement('div');
+    div.style = 'margin-right:20px'
     const screenSize = window.innerWidth;
     if(screenSize < 800){
       div.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="none" viewBox="0 0 40 40"><path fill="#26D782" d="M20 5a15 15 0 1 1 0 30 15 15 0 0 1 0-30Zm0 2.5a12.5 12.5 0 1 0 0 25 12.5 12.5 0 0 0 0-25Zm-1.875 15.105L25.3 15.41a1.25 1.25 0 0 1 1.915 1.593l-.145.174-8.06 8.08a1.25 1.25 0 0 1-1.595.148l-.175-.145-4.375-4.375a1.25 1.25 0 0 1 1.595-1.913l.175.143 3.49 3.49Z"/></svg>'
@@ -116,22 +120,35 @@ const answerIsCorrect = (correctAnswer, selectedAnswer, elementSelected) => {
       div.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="none" viewBox="0 0 40 40"><path fill="#26D782" d="M20 5a15 15 0 1 1 0 30 15 15 0 0 1 0-30Zm0 2.5a12.5 12.5 0 1 0 0 25 12.5 12.5 0 0 0 0-25Zm-1.875 15.105L25.3 15.41a1.25 1.25 0 0 1 1.915 1.593l-.145.174-8.06 8.08a1.25 1.25 0 0 1-1.595.148l-.175-.145-4.375-4.375a1.25 1.25 0 0 1 1.595-1.913l.175.143 3.49 3.49Z"/></svg>'
     }
     elementSelected.parentNode.appendChild(div)
-    console.log('CORRECT!')
+    submitAnswer.textContent = 'Next Question'
   }else{
-    console.log('INCORRECT!')
+     // INCORRECT ANSWER
+    elementSelected.parentNode.style = 'border: 2px solid #EE5454; justify-content:space-between;';
+    elementSelected.previousElementSibling.style.backgroundColor = '#EE5454';
+    const div = document.createElement('div');
+    div.style = 'margin-right:20px'
+    const screenSize = window.innerWidth;
+    if(screenSize < 800){
+      div.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="none" viewBox="0 0 40 40"><path fill="#EE5454" d="M20 5a15 15 0 1 1 0 30 15 15 0 0 1 0-30Zm0 2.5a12.5 12.5 0 1 0 0 25 12.5 12.5 0 0 0 0-25Zm-5.402 7.415.142-.175a1.25 1.25 0 0 1 1.595-.143l.175.143L20 18.233l3.49-3.493a1.25 1.25 0 0 1 1.595-.143l.175.143a1.25 1.25 0 0 1 .142 1.595l-.142.175L21.767 20l3.493 3.49a1.25 1.25 0 0 1 .142 1.595l-.142.175a1.25 1.25 0 0 1-1.595.142l-.175-.142L20 21.767l-3.49 3.493a1.25 1.25 0 0 1-1.595.142l-.175-.142a1.25 1.25 0 0 1-.143-1.595l.143-.175L18.233 20l-3.493-3.49a1.25 1.25 0 0 1-.143-1.595Z"/></svg>'
+
+    }else{
+      div.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="none" viewBox="0 0 40 40"><path fill="#EE5454" d="M20 5a15 15 0 1 1 0 30 15 15 0 0 1 0-30Zm0 2.5a12.5 12.5 0 1 0 0 25 12.5 12.5 0 0 0 0-25Zm-5.402 7.415.142-.175a1.25 1.25 0 0 1 1.595-.143l.175.143L20 18.233l3.49-3.493a1.25 1.25 0 0 1 1.595-.143l.175.143a1.25 1.25 0 0 1 .142 1.595l-.142.175L21.767 20l3.493 3.49a1.25 1.25 0 0 1 .142 1.595l-.142.175a1.25 1.25 0 0 1-1.595.142l-.175-.142L20 21.767l-3.49 3.493a1.25 1.25 0 0 1-1.595.142l-.175-.142a1.25 1.25 0 0 1-.143-1.595l.143-.175L18.233 20l-3.493-3.49a1.25 1.25 0 0 1-.143-1.595Z"/></svg>'
+    }
+    elementSelected.parentNode.appendChild(div)
+    submitAnswer.textContent = 'Next Question'
   }
 }
 
 // CORRECT! 
-// change the color of border to green
-// the letter box color and add the icon
-// FIX THE Padding
+// change the color of border to green ok
+// the letter box color and add the icon ok
+// FIX THE Padding ok
 
 //INCORRECT!
-// change the color of border to red
-// the letter box color and add the icon
+// change the color of border to OKok
+// the letter box color and add the icon ok
 
-//CHange the SUBMit  BTN to next question
+//CHange the SUBMit  BTN to next question ok
 
 // If the user click on submit btn before selected send a alert
 
