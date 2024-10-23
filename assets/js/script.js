3c044ad1b719db1a13debf5564b88e64d22fa9c8
Weverson Godinho <weverson.godinho@icloud.com>
2024-10-23 15:08:07 +0000

    Fix the bug on submit function

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,11 @@ const questionsHeanding = document.querySelector('.questions h1');
 const btnOptionsParagraph = document.querySelectorAll('.answers p');
 const btnOptions = document.querySelectorAll('.answers');
 const submitAnswer = document.querySelector('#submit-answer');
+const multChoice = document.querySelector('#mult-choice');
 
 let correctAnswer;
+let selectedAnswer = null; 
+let elementSelected = null; 
 
 // SLIDER
 const range = () =>{
@@ -32,14 +35,12 @@ const btnValue = () =>{
 }
 btnValue()
 
-
 // Get the Subject selected from localStorage
 const selectedSubject = localStorage.getItem('selectedSubject');
 if(selectedSubject){
   localStorage.removeItem('selectedSubject');
 }
    
-
 // . Get the data from Json 
 let data;
 fetch('./data.json')
@@ -72,39 +73,68 @@ const displayQuestions = (questions) => {
     btnOptionsParagraph[i].innerHTML = questions.options[i]
   }
   getValueBtnOptions(btnOptionsParagraph)
-  
 }
 
 // Get value from Answer selected
-
-const getValueBtnOptions = (answer)=>{
+const getValueBtnOptions = (answer) => {
   answer.forEach(el => {
-    el.addEventListener('click', () =>{
-      
-      //RESET ALL VALUES
-      answer.forEach((btn)=>{
-        btn.parentNode.style = ''
-        btn.previousElementSibling.style.backgroundColor = '';
-        btn.previousElementSibling.style.color = '';
-      })
-      let elementSelected = el;
-      let selectedAnswer = el.textContent
-      submitAnswerBtn(elementSelected,selectedAnswer)
-      el.parentNode.style = 'border: 2px solid #A729F5;'
+    el.addEventListener('click', () => {
+      // RESET ALL VALUES
+      answer.forEach((btn) => {
+        btn.parentNode.style = ''; // Reset border
+        btn.previousElementSibling.style.backgroundColor = ''; // Reset background
+        btn.previousElementSibling.style.color = ''; // Reset text color
+      });
+
+      elementSelected = el; // Set the selected element
+      selectedAnswer = el.textContent; // Set the selected answer
+
+      // Apply styles to the selected element
+      el.parentNode.style.border = '2px solid #A729F5';
       el.previousElementSibling.style.backgroundColor = '#A729F5';
-      el.previousElementSibling.style.color = '#fff ';
+      el.previousElementSibling.style.color = '#fff';
     });
-  
   });
 };
 
-// Submit the answer
-const submitAnswerBtn = (elementSelected,selectedAnswer) =>{
-  submitAnswer.addEventListener('click',() =>{
-    answerIsCorrect(correctAnswer, selectedAnswer, elementSelected)
-  
-  })
+// This If statement checks if the submitAnswer element exists 
+// to avoid errors when trying to add an event listener to a null element
+if(submitAnswer){
+
+    // Submit the answer
+  const submitAnswerBtn = () => {
+    submitAnswer.addEventListener('click', () => {
+      if (!selectedAnswer) {
+        const screenSize = window.innerWidth;
+        const div = document.createElement('div');
+        div.style = 'display:flex; justify-content:center; align-items:flex-end; width:100%; margin-top:32px';
+        
+        const p = document.createElement('p');
+        p.style = 'color:#EE5454'
+        p.innerHTML = 'Please select an answer';
+
+        div.appendChild(p);
+
+        const divSvg = document.createElement('div');
+        if(screenSize < 800){
+          divSvg.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="none" viewBox="0 0 40 40"><path fill="#EE5454" d="M20 5a15 15 0 1 1 0 30 15 15 0 0 1 0-30Zm0 2.5a12.5 12.5 0 1 0 0 25 12.5 12.5 0 0 0 0-25Zm-5.402 7.415.142-.175a1.25 1.25 0 0 1 1.595-.143l.175.143L20 18.233l3.49-3.493a1.25 1.25 0 0 1 1.595-.143l.175.143a1.25 1.25 0 0 1 .142 1.595l-.142.175L21.767 20l3.493 3.49a1.25 1.25 0 0 1 .142 1.595l-.142.175a1.25 1.25 0 0 1-1.595.142l-.175-.142L20 21.767l-3.49 3.493a1.25 1.25 0 0 1-1.595.142l-.175-.142a1.25 1.25 0 0 1-.143-1.595l.143-.175L18.233 20l-3.493-3.49a1.25 1.25 0 0 1-.143-1.595Z"/></svg>'
+    
+        }else{
+          divSvg.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="none" viewBox="0 0 40 40"><path fill="#EE5454" d="M20 5a15 15 0 1 1 0 30 15 15 0 0 1 0-30Zm0 2.5a12.5 12.5 0 1 0 0 25 12.5 12.5 0 0 0 0-25Zm-5.402 7.415.142-.175a1.25 1.25 0 0 1 1.595-.143l.175.143L20 18.233l3.49-3.493a1.25 1.25 0 0 1 1.595-.143l.175.143a1.25 1.25 0 0 1 .142 1.595l-.142.175L21.767 20l3.493 3.49a1.25 1.25 0 0 1 .142 1.595l-.142.175a1.25 1.25 0 0 1-1.595.142l-.175-.142L20 21.767l-3.49 3.493a1.25 1.25 0 0 1-1.595.142l-.175-.142a1.25 1.25 0 0 1-.143-1.595l.143-.175L18.233 20l-3.493-3.49a1.25 1.25 0 0 1-.143-1.595Z"/></svg>'
+        }
+        
+    
+        div.insertBefore(divSvg,p)
+        multChoice.appendChild(div)
+        return;
+      }
+      // Check if the selected answer is correct
+      answerIsCorrect(correctAnswer, selectedAnswer, elementSelected);
+    });
+  }
+  submitAnswerBtn()
 }
+
 
 const answerIsCorrect = (correctAnswer, selectedAnswer, elementSelected) => {
   if(correctAnswer.answer === selectedAnswer){
